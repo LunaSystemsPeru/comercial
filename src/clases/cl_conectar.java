1abcbe1fa084bb6b4d05497be1094f0cf61bcb7b
Luis <leog.1992@gmail.com>
2022-07-18 17:09:41 -0500

    cambio de servidor

--- src/clases/cl_conectar.java
+++ src/clases/cl_conectar.java
@@ -16,10 +16,10 @@ import javax.swing.JOptionPane;
 public class cl_conectar {
 
     private static Connection conexion = null;
-    private static String bd = "comercial_penia"; // Nombre de BD.
-    private static String user = "lsp_comercial"; // Usuario de BD.
-    private static String password = "KEvNlFS2ro7Q"; // Password de BD.
-    private static String url = "lunasystemsperu.com";
+    private static String bd = "brunoasc_comercial_penia"; // Nombre de BD.
+    private static String user = "brunoasc_luis_bd"; // Usuario de BD.
+    private static String password = "C]6&TN4Bt@&I"; // Password de BD.
+    private static String url = "artemisa.servidoresph.com";
     //private static String url = "localhost";
 
     // Driver para MySQL en este caso.
